73dc44d12d7858971ceb86ddddeea97ed2eef0a6
Guillaume Prevost <guillaume.prevost@rmit.edu.au>
2017-06-02 14:08:38 +1000

    Various ports now part of the config (i.e. not hardcoded). eStoRED waits for MySQL DB to be up.

--- estored/scripts/run.sh
+++ estored/scripts/run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-openssl x509 -in <(openssl s_client -connect $ESTORED_KENLET_REPOSITORY_HOST:443 -prexit 2>/dev/null) -out ~/kenlet-repository-ssl-certificate.crt
+openssl x509 -in <(openssl s_client -connect $ESTORED_KENLET_REPOSITORY_HOST:$ESTORED_KENLET_REPOSITORY_SSL_PORT -prexit 2>/dev/null) -out ~/kenlet-repository-ssl-certificate.crt
 keytool -importcert -noprompt -file ~/kenlet-repository-ssl-certificate.crt -alias kenlet-repo -keystore $JAVA_HOME/jre/lib/security/cacerts -storepass changeit
 
-java -Djava.security.egd=file:/dev/./urandom -Xdebug -Xrunjdwp:server=y,transport=dt_socket,suspend=n -jar /estored/estored.jar --spring.profiles.active=prod
+# Wait for the MySQL server to be up and running
+/scripts/waitforit.sh -t 240 $MYSQL_HOST:$MYSQL_PORT -- echo "MySQL DB is up"
+
+java -jar /estored/estored.jar --spring.profiles.active=prod
